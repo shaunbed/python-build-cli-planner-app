1799b0f67a8ce7b62932fa8978c23d1b1cb335e4
Shaun Bedingfield <10125315+shaunbed@users.noreply.github.com>
2023-04-03 09:25:10 -0500

    3

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -3,6 +3,6 @@ from collections.abc import Iterable
 
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
-    @abstractmethod()
-    def is_due():
+    @abstractmethod
+    def is_due(self):
         pass
